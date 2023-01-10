cc747a3ed7bd27cdc15546a21b4894790aae7cc6
Scherbakova <nastyasch0806@gmail.com>
2023-01-10 22:18:05 +0300

    commit алгоритмы вторая часть

--- src/test/java/skypro/com/algorithms/AlgorithmsTest.java
+++ src/test/java/skypro/com/algorithms/AlgorithmsTest.java
@@ -8,21 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AlgorithmsTest {
 
-    private StringListImpl expected;
-    private StringListImpl actual;
+    private IntegerListImpl expected;
+    private IntegerListImpl actual;
 
     @BeforeEach
     public void setUp() {
-        expected = new StringListImpl(10);
-        String string1 = "s1";
-        String string2 = "s2";
-        String string3 = "s3";
-        String string4 = "s4";
+        expected = new IntegerListImpl(10);
+        Integer string1 = Integer.valueOf("s1");
+        Integer string2 = Integer.valueOf("s2");
+        Integer string3 = Integer.valueOf("s3");
+        Integer string4 = Integer.valueOf("s4");
         expected.add(string1);
         expected.add(string2);
         expected.add(string3);
         expected.add(string4);
-        actual = new StringListImpl(10);
+        actual = new IntegerListImpl(10);
         actual.add(string1);
         actual.add(string2);
         actual.add(string3);
@@ -31,12 +31,12 @@ public class AlgorithmsTest {
 
     @Test
     public void addString() {
-        StringListImpl stringList = new StringListImpl(10);
-        String string1 = "s1";
-        String string2 = "s2";
-        String string3 = "s3";
-        String string4 = "s4";
-        stringList.add(string1);
+        IntegerListImpl stringList = new IntegerListImpl(10);
+        Integer string1 = Integer.valueOf("s1");
+        Integer string2 = Integer.valueOf("s2");
+        Integer string3 = Integer.valueOf("s3");
+        Integer string4 = Integer.valueOf("s4");
+        stringList.add(Integer.valueOf(string1));
         stringList.add(string2);
         stringList.add(string3);
         stringList.add(string4);
